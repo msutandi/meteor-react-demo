b445bfb608e588f694705403a7c875bcc1dc259f
Michael Sutandi <michael.sutandi@turner.com>
2015-10-06 16:02:07 -0400

    removed velocity

--- tests/jasmine/client/integration/simple-todos-reactSpec.js
+++
@@ -1,9 +0,0 @@
-// tests/jasmine/client/integration/simple-todos-reactSpec.js
-
-describe("simple-todos-react", function(){
-  describe("head", function(){
-    it("shows 'Todo List' title", function(){
-      expect($('title').text()).toEqual('Todo List');
-    });
-  });
-});
